5f919d8e58357306636834ecc305151edc1c99e6
pedro <pedro@pedro-Despacho>
2026-09-23 12:21:01 +0200

    añadir archivo __init__.py

--- python_tabla/python_tabla/__init__.py
+++ python_tabla/python_tabla/__init__.py
@@ -1,7 +1,8 @@
-from python_tabla.desplazamiento_tabla import Desplazamiento
-from python_tabla.edicion_tabla import TablaEdicion
-from python_tabla.tabla import Tabla
+"""
+Python_Tabla - Estructura de tabla para acceso a tablas de bases de datos
+"""
+from python_tabla.tabla import *
+from python_tabla.edicion_tabla import *
+from python_tabla.desplazamiento_tabla import *
 
-
-# pip3 install build
-# pip3 install .
+__all__ = []
